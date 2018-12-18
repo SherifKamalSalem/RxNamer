b75551cf1994e088752995f614594be8130a38ec
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-12-18 11:37:35 +0200

    Initial Commit

---
+++ RxNamer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxNamer
+//
+//  Created by Sherif Kamal on 12/18/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
